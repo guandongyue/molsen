728a36d02050bd8d76f0166a458f24d69103d583
Tanton.Green <guandongyue@gmail.com>
2017-12-01 17:23:41 +0800

    11

--- resources/views/article/view.blade.php
+++ resources/views/article/view.blade.php
@@ -2,25 +2,25 @@
 
 @section('content')
 
-        <div class="blog-post"> 
-          <h2 class="blog-post-title">{{ $article->title }}</h2>
-          
-          <div class="row">
-            <div class="col-sm-8">
-              <p class="blog-post-meta">{{ $article->intime }} by <a href="#">Mark</a></p>
-            </div>
-            <div class="col-sm-4 text-right">
-            @auth
-              <a class="btn btn-default" href="/admin/article/edit/{{ $article->id }}" role="button">编辑</a>
-            @endauth
-            </div>
-          </div>
+<div class="blog-post"> 
+  <h2 class="blog-post-title">{{ $article->title }}</h2>
+  
+  <div class="row">
+    <div class="col-sm-8">
+      <p class="blog-post-meta">{{ $article->intime }} by <a href="#">Mark</a></p>
+    </div>
+    <div class="col-sm-4 text-right">
+    @auth
+      <a class="btn btn-default" href="/admin/article/edit/{{ $article->id }}" role="button">编辑</a>
+    @endauth
+    </div>
+  </div>
 
-          <p>{{ $article->description }}</p>
-          <hr>
-          <div id="doc-content" class="none-padding">
-            <textarea style="display:none;">{{ $article->note }}</textarea>
-          </div>
-        </div><!-- /.blog-post -->
+  <p>{{ $article->description }}</p>
+  <hr>
+  <div id="doc-content" class="none-padding">
+    <textarea style="display:none;">{{ $article->note }}</textarea>
+  </div>
+</div><!-- /.blog-post -->
 
 @endsection
