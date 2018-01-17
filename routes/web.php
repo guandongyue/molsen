8fd9ad79f4e5db075897ece06f0753ab2b57dd58
Tanton.Green <guandongyue@gmail.com>
2018-01-17 20:31:31 +0800

    git pull update

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,6 @@ Route::get('/tag/{tag?}', 'BlogController@tag');
 Route::get('/article/{id}', 'BlogController@view');
 Route::get('/article/category/{categoryid}', 'BlogController@list');
 
-Route::get('/test/{debug?}', function ($debug=null){
-    if ($debug=='1314521') {
-        phpinfo();
-        exit;
-    }
-    return Redirect::to('/');
-});
-
 Route::get('/article/edit/{id}', 'BlogController@edit')->middleware('auth');
 Route::get('/article/delete/{id}', 'BlogController@delete')->middleware('auth');
 Route::post('/article/save', 'BlogController@save')->middleware('auth');
@@ -47,4 +39,39 @@ Route::Group(['middleware'=>'auth', 'prefix' => 'admin', 'namespace' => 'Admin']
     Route::get('/article/edit/{id?}', 'ArticleController@edit');
     Route::post('/article/save', 'ArticleController@save');
     Route::post('/article/delete', 'ArticleController@delete');
+});
+
+
+Route::get('/update', function ($debug=null){
+    $hook_log = file_get_contents('php://input');
+
+    if (!empty($hook_log)) {
+        // 是WebHook请求, 并decode数据
+        $json = json_decode($hook_log, true);
+        if (array_key_exists('ref', $json)) {
+            system('/usr/bin/sudo git pull 2>&1', $retval);
+        } else {
+        }
+    } else {
+        // 直接访问URL，手动更新
+        echo '<pre>';
+    
+        $last_line = system('/usr/bin/sudo git pull 2>&1', $retval);
+    
+        // 打印更多信息
+        echo "
+        </pre>
+        <hr />Last line of the output: {$last_line}
+        <hr />Return value: {$retval}
+        <hr />手动更新";
+    }
+});
+
+
+Route::get('/test/{debug?}', function ($debug=null){
+    if ($debug=='1314521') {
+        phpinfo();
+        exit;
+    }
+    return Redirect::to('/');
 });
